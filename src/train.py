a6caffdac950668405cc46be17d7d5cfc8c42eb6
Rinat Dautbaev <rdautbayev@gmail.com>
2026-07-01 10:37:06 +0000

    Add GitHub Actions CI

---
+++ src/train.py
@@ -0,0 +1,13 @@
+import sys
+import torch
+
+print("=" * 50)
+print("MLOps Lab")
+print("=" * 50)
+
+print(f"Python version : {sys.version}")
+print(f"PyTorch version: {torch.__version__}")
+
+print("\nEnvironment is ready!")
+
+print("=" * 50)
